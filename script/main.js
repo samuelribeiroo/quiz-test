bc84b27e797890c48e13ad2a4718dc3755724c3b
samuelribeiroo <samuelribeirodev@gmail.com>
2024-05-24 15:40:40 -0300

    update(json): array with questions updated

--- script/main.js
+++ script/main.js
@@ -9,88 +9,109 @@ const perguntas = [
     correta: 1,
   },
   {
-    pergunta: "Qual é a função do operador '===' em comparações em JavaScript?",
-    respostas: [
-      "Comparação de valores sem considerar o tipo",
-      "Atribuição de valores",
-      "Comparação estrita de valores e tipos",
-    ],
-    correta: 2,
+    pergunta:
+      "Qual o método correto para adicionar um elemento no final de um array em JavaScript?",
+    respostas: ["array.push()", "array.pop()", "array.shift()"],
+    correta: 0,
   },
   {
-    pergunta: "Como se declara uma variável em JavaScript?",
-    respostas: [
-      "let myVar;",
-      "const myVar = 10;",
-      "ambas as opções acima estão corretas",
-    ],
-    correta: 2,
-  },
-  {
-    pergunta: "O que é uma função em JavaScript?",
-    respostas: [
-      "Um tipo de dado",
-      "Um bloco de código reutilizável",
-      "Uma variável global",
-    ],
+    pergunta:
+      "Em Node.js, qual módulo é utilizado para lidar com o sistema de arquivos?",
+    respostas: ["http", "fs", "path"],
     correta: 1,
   },
   {
     pergunta:
-      "Qual é a diferença entre 'let' e 'const' na declaração de variáveis?",
+      "Qual comando do npm é usado para instalar um pacote e salvá-lo como uma dependência de desenvolvimento?",
     respostas: [
-      "Nenhuma, são sinônimos",
-      "let é usado para valores constantes, const para variáveis",
-      "let permite reatribuição, const cria variáveis imutáveis",
+      "npm install <pacote> --save",
+      "npm install <pacote> --dev",
+      "npm install <pacote> --save-dev",
     ],
     correta: 2,
   },
   {
-    pergunta: "O que é o DOM em JavaScript?",
+    pergunta:
+      "Qual hook do React é usado para manipular o estado em componentes funcionais?",
+    respostas: ["useState", "useEffect", "useReducer"],
+    correta: 0,
+  },
+  {
+    pergunta:
+      "Qual a função do método 'componentDidMount()' em um componente de classe React?",
     respostas: [
-      "Um método de criptografia",
-      "Um modelo de objeto para manipular documentos HTML",
-      "Uma linguagem de programação",
+      "Atualizar o DOM após a montagem",
+      "Renderizar o componente",
+      "Fazer uma limpeza antes de desmontar o componente",
     ],
+    correta: 0,
+  },
+  {
+    pergunta:
+      "Em JavaScript, qual operador é utilizado para comparar tanto o valor quanto o tipo?",
+    respostas: ["==", "===", "="],
     correta: 1,
   },
   {
     pergunta:
-      "Como se realiza uma iteração sobre os elementos de um array em JavaScript?",
+      "Qual das opções a seguir é uma maneira correta de criar um servidor HTTP em Node.js?",
     respostas: [
-      "Usando a estrutura 'if-else'",
-      "Com a declaração 'switch'",
-      "Utilizando loops como 'for' ou 'forEach'",
+      "http.createServer()",
+      "fs.createServer()",
+      "net.createServer()",
     ],
+    correta: 0,
+  },
+  {
+    pergunta:
+      "Em React, qual hook é usado para executar efeitos colaterais em componentes funcionais?",
+    respostas: ["useContext", "useRef", "useEffect"],
     correta: 2,
   },
   {
-    pergunta: "O que é o JSON em JavaScript?",
+    pergunta:
+      "Qual das seguintes opções não é um ciclo de vida de um componente React?",
     respostas: [
-      "Um método de formatação de texto",
-      "Uma linguagem de estilização",
-      "Um formato de dados leve e intercambiável",
+      "componentWillMount",
+      "componentDidUpdate",
+      "componentWillUpdate",
     ],
-    correta: 2,
+    correta: 0,
   },
   {
-    pergunta: "Qual é a diferença entre 'null' e 'undefined' em JavaScript?",
+    pergunta: "Qual é a função da tag '<title>' em um documento HTML?",
     respostas: [
-      "São iguais, usados de forma intercambiável",
-      "'null' representa a ausência de valor, 'undefined' é atribuído explicitamente",
-      "Ambos representam valores vazios",
+      "Definir o título do documento",
+      "Criar um parágrafo",
+      "Linkar uma folha de estilo",
     ],
+    correta: 0,
+  },
+  {
+    pergunta: "Qual tag HTML é usada para criar um link?",
+    respostas: ["<link>", "<a>", "<href>"],
     correta: 1,
   },
   {
     pergunta:
-      "Como se adiciona um evento a um elemento HTML usando JavaScript?",
+      "Qual propriedade CSS é usada para mudar a cor de fundo de um elemento?",
+    respostas: ["color", "background-color", "font-color"],
+    correta: 1,
+  },
+  {
+    pergunta: "Qual é a finalidade da propriedade 'flex' no CSS Flexbox?",
     respostas: [
-      "Apenas com CSS",
-      "Usando o atributo 'event'",
-      "Através do método 'addEventListener'",
+      "Definir o alinhamento de itens",
+      "Definir a direção do layout",
+      "Definir como um item vai crescer ou encolher",
     ],
     correta: 2,
+  },
+  {
+    pergunta:
+      "Qual unidade de medida em CSS é relativa ao tamanho da fonte do elemento?",
+    respostas: ["em", "px", "cm"],
+    correta: 0,
   },
 ];
 
@@ -129,6 +150,3 @@ for (let item of perguntas) {
   quizItem.querySelector("dl dt").remove();
   quiz.appendChild(quizItem);
 }
-
-console.log(template, quizItem);
-
