2814b6f9be170251bf6272811c749cad32f0b210
samuelribeiroo <samuelribeirodev@gmail.com>
2024-07-08 14:23:55 -0300

    refactor: JSON in a single file

---
+++ script/databse.js
@@ -0,0 +1,116 @@
+export const questions = [
+  {
+    pergunta: "Qual é a finalidade do comando 'console.log()' em JavaScript?",
+    respostas: [
+      "Exibir uma mensagem de erro",
+      "Imprimir dados no console",
+      "Criar uma variável",
+    ],
+    correta: 1,
+  },
+  {
+    pergunta:
+      "Qual o método correto para adicionar um elemento no final de um array em JavaScript?",
+    respostas: ["array.push()", "array.pop()", "array.shift()"],
+    correta: 0,
+  },
+  {
+    pergunta:
+      "Em Node.js, qual módulo é utilizado para lidar com o sistema de arquivos?",
+    respostas: ["http", "fs", "path"],
+    correta: 1,
+  },
+  {
+    pergunta:
+      "Qual comando do npm é usado para instalar um pacote e salvá-lo como uma dependência de desenvolvimento?",
+    respostas: [
+      "npm install <pacote> --save",
+      "npm install <pacote> --dev",
+      "npm install <pacote> --save-dev",
+    ],
+    correta: 2,
+  },
+  {
+    pergunta:
+      "Qual hook do React é usado para manipular o estado em componentes funcionais?",
+    respostas: ["useState", "useEffect", "useReducer"],
+    correta: 0,
+  },
+  {
+    pergunta:
+      "Qual a função do método 'componentDidMount()' em um componente de classe React?",
+    respostas: [
+      "Atualizar o DOM após a montagem",
+      "Renderizar o componente",
+      "Fazer uma limpeza antes de desmontar o componente",
+    ],
+    correta: 0,
+  },
+  {
+    pergunta:
+      "Em JavaScript, qual operador é utilizado para comparar tanto o valor quanto o tipo?",
+    respostas: ["==", "===", "="],
+    correta: 1,
+  },
+  {
+    pergunta:
+      "Qual das opções a seguir é uma maneira correta de criar um servidor HTTP em Node.js?",
+    respostas: [
+      "http.createServer()",
+      "fs.createServer()",
+      "net.createServer()",
+    ],
+    correta: 0,
+  },
+  {
+    pergunta:
+      "Em React, qual hook é usado para executar efeitos colaterais em componentes funcionais?",
+    respostas: ["useContext", "useRef", "useEffect"],
+    correta: 2,
+  },
+  {
+    pergunta:
+      "Qual das seguintes opções não é um ciclo de vida de um componente React?",
+    respostas: [
+      "componentWillMount",
+      "componentDidUpdate",
+      "componentWillUpdate",
+    ],
+    correta: 0,
+  },
+  {
+    pergunta: "Qual é a função da tag '<title>' em um documento HTML?",
+    respostas: [
+      "Definir o título do documento",
+      "Criar um parágrafo",
+      "Linkar uma folha de estilo",
+    ],
+    correta: 0,
+  },
+  {
+    pergunta: "Qual tag HTML é usada para criar um link?",
+    respostas: ["<link>", "<a>", "<href>"],
+    correta: 1,
+  },
+  {
+    pergunta:
+      "Qual propriedade CSS é usada para mudar a cor de fundo de um elemento?",
+    respostas: ["color", "background-color", "font-color"],
+    correta: 1,
+  },
+  {
+    pergunta: "Qual é a finalidade da propriedade 'flex' no CSS Flexbox?",
+    respostas: [
+      "Definir o alinhamento de itens",
+      "Definir a direção do layout",
+      "Definir como um item vai crescer ou encolher",
+    ],
+    correta: 2,
+  },
+  {
+    pergunta:
+      "Qual unidade de medida em CSS é relativa ao tamanho da fonte do elemento?",
+    respostas: ["em", "px", "cm"],
+    correta: 0,
+  },
+];
